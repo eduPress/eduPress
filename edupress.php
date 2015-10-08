9c8747cdbcc246fd1b089fca073c6f54b68c02ab
Rodrigo Sousa <rodrigofrcs@hotmail.com>
2015-10-08 14:18:49 -0300

    Commit inicial

---
+++ edupress.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * The eduPress Plugin
+ *
+ * eduPress is EAD software from the creators of WordPress.
+ *
+ * @package eduPress
+ * @subpackage Main
+ */
+
+/**
+ * Plugin Name: eduPress
+ * Plugin URI:  https://github.com/eduPress/eduPress
+ * Description: eduPress is EAD software from the creators of WordPress.
+ * Author:      Francisco Rodrigo Cunha de Sousa
+ * Author URI:  http://rodrigosousa.info
+ * Version:     1.0.0
+ * Text Domain: edupress
+ * Domain Path: /languages/
+ */
